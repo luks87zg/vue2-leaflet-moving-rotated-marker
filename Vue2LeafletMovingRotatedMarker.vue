790064cd16c64e641f9dd81b085c49523659777f
helly-fox <elena.proweb@gmail.com>
2023-04-21 16:27:25 +0300

    Fix call order (#2)
    Co-authored-by: alsa <alsa@gurtam.com>

--- Vue2LeafletMovingRotatedMarker.vue
+++ Vue2LeafletMovingRotatedMarker.vue
@@ -76,11 +76,11 @@
             })
             L.DomEvent.on(this.mapObject, this.$listeners)
             propsBinder(this, this.mapObject, props)
+            this.parentContainer = findRealParent(this.$parent)
             // Check if has cluster marker as parent and override parent container to skip clustering
             if (this.$parent.$vnode.componentOptions.tag.includes("-marker-cluster")) {
                 this.parentContainer = this.parentContainer.parentContainer;
             }
-            this.parentContainer = findRealParent(this.$parent)
             this.parentContainer.addLayer(this, !this.visible)
             this.ready = true
             this.$nextTick(() => {
